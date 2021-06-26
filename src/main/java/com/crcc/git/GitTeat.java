96f7f3085452d98a9ac50cbcbad879fc99d6a2a9
long <203881390@qq.com>
2021-06-27 00:48:40 +0800

    first commit

---
+++ src/main/java/com/crcc/git/GitTeat.java
@@ -0,0 +1,16 @@
+package com.crcc.git;
+
+/**
+ * ClassName:GitTeat
+ * Package:com.crcc.git
+ * Description:
+ *
+ * @date:2021/6/27 0:24
+ * @author:319048285@qq.com
+ */
+public class GitTeat {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
